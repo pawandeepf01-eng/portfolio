f2bcb85441fbb64f7caf8a35344e0ea334e2d423
pawandeepf01-eng <pawandeep.f01@gmail.com>
2026-03-12 16:01:31 +0530

    change in navbar

--- src/components/Nav-bar.jsx
+++ src/components/Nav-bar.jsx
@@ -8,15 +8,22 @@ import Logo from "../assets/princeimg.jpeg";
 export default function Navbar() {
   return (
     <div className="w-full h-16 px-4 bg-blue-400 text-white flex items-center justify-between">
+      <img src={Logo} alt="" className="h-12 w-12 rounded-full" />
 
-      <img src={Logo} alt="" className="h-12 w-12 rounded-full"/>
-
-      <div className="hidden sm:flex h-15 w-200 justify-evenly items-center text-sm sm:text-base font-bold gap-6">
-        <Link to="/" className="hover:translate-y-1 hover:text-sky-800">Home</Link>
-        <Link to="/about" className="hover:translate-y-1 hover:text-sky-800">About</Link>
-        <Link to="/resume" className="hover:translate-y-1 hover:text-sky-800">Resume</Link>
+      <div className="flex h-15 w-200 justify-evenly items-center text-sm sm:text-base font-bold gap-6">
+        <Link to="/" className="hover:translate-y-1 hover:text-sky-800">
+          Home
+        </Link>
+        <Link to="/about" className="hover:translate-y-1 hover:text-sky-800">
+          About
+        </Link>
+        <Link to="/resume" className="hover:translate-y-1 hover:text-sky-800">
+          Resume
+        </Link>
         {/* <Link to="/service" className="hover:translate-y-1 hover:text-sky-800">Work</Link> */}
-        <Link to="/contact" className="hover:translate-y-1 hover:text-sky-800">Contact</Link>
+        <Link to="/contact" className="hover:translate-y-1 hover:text-sky-800">
+          Contact
+        </Link>
       </div>
 
       <div className="flex gap-3 sm:gap-4 items-center">
